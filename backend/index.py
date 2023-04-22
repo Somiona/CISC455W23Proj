e9cdf7aad928a3a4deafa3027c31de4e0aeaa0e4
Somiona <somionat@gmail.com>
2023-04-22 15:40:43 -0400

    update

--- backend/index.py
+++ backend/index.py
@@ -5,3 +5,19 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/nextgen")
+def trigger_next_generation():
+    return "<p>Next generation triggered!</p>"
+
+@app.route("/nextgen/<int:generation>")
+def get_next_generation(generation):
+    return f"<p>Next generation: {generation}</p>"
+
+@app.route("/fibonacci/<int:n>")
+def calculate_nth_fibonacci(n):
+    # calculate nth fibonacci number
+    return f"<p>{n}th fibonacci number is {recur_fib(n)}</p>"
+
+def recur_fib(n):
+    return n if n <= 1 else (recur_fib(n-1) + recur_fib(n-2))
--- launch_all.ps1
+++ launch_all.ps1
@@ -1,4 +1,2 @@
-$nodeProcess = Start-Process deno -ArgumentList "task --cwd .\\frontend -c .\\frontend\\deno.json start" -PassThru
-
-# Launch Flask server
-$flaskProcess = Start-Process pipenv -ArgumentList "run flask --app .\\backend\\index run" -PassThru
+Start-Process deno -ArgumentList "task --cwd .\\frontend -c .\\frontend\\deno.json start"
+Start-Process pipenv -ArgumentList "run flask --app .\\backend\\index run"
