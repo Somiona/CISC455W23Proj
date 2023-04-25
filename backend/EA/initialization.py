603292c63873eeee5296467c590ebf9313e80ade
codeZ-1627 <echingkho@Es-MacBook-Pro.cogeco.local>
2023-04-25 05:58:35 -0400

    update: improve initialization and finish Action Rundown component

--- backend/EA/initialization.py
+++ backend/EA/initialization.py
@@ -17,6 +17,7 @@ class Share:
         self.price = price
         self.owner = None
         self.history = {"price":[], "owner":[]}
+        self.failed_selling = 0
     
     def update_history(self):
         """
@@ -25,6 +26,8 @@ class Share:
         """
         self.history["price"].append(self.price)
         self.history["owner"].append(self.owner)
+        self.failed_selling = 0
+
 
     def update_owner(self, investor):
         """
@@ -33,7 +36,7 @@ class Share:
         For initalization purpose, to assign share to investor
         """
         self.owner = investor
-        investor.shares.append(self)
+        investor.shares[self.stock.name].append(self)
         self.update_history()
 
 
@@ -47,43 +50,76 @@ class Share:
         self.price = price
         self.update_history()
 
+
     def __repr__(self) -> str:
-        return f'{self.stock.id}:{self.id}-($:{self.price},under:{self.owner})'
+        return f'{self.stock.name}:{self.id}-($:{self.price},under:{self.owner})'
 
 
 class Stock:
-    def __init__(self, id, num_shares, num_gen, p_stock_mean, p_stock_sd, trend_length, p_share_sd):
-        self.id = id
+    def __init__(self, name, num_shares, num_gen, p_stock_mean, p_stock_sd, scope_length, p_share_sd):
+        self.name = name
         self.shares = []
         self.num_shares = num_shares
-        self.price_history = np.concatenate((np.random.normal(p_stock_mean, p_stock_sd, trend_length), np.zeros(num_gen))) # Initialize price history wth random trend
+        self.scope_length = scope_length
+        self.price_history = np.concatenate((np.round(np.random.normal(p_stock_mean, p_stock_sd, scope_length),2), np.zeros(num_gen))) # Initialize price history wth random trend
 
         # Generate Shares
         for i in range(num_shares):
-            self.shares.append(Share(id=i, stock=self, price=np.random.normal(self.price_history[trend_length-1], p_share_sd))) # shares' price is based on the last tu of random trend
+            self.shares.append(Share(id=i, stock=self, price=np.round(np.random.normal(self.price_history[scope_length-1], p_share_sd),2))) # shares' price is based on the last tu of random trend
 
-        # Replace last trend value
+        # Replace last value in the scope
         total_price = 0
         for s in self.shares:
             total_price += s.price
-        self.price_history[trend_length-1] = total_price/num_shares
+        self.price_history[scope_length-1] = np.round(total_price/num_shares,2)
+
+        # Obtain previous data information
+        self.previous_data = self.get_stock_data(0)
+        self.average_scope = self.get_average_scope()
+        self.last_price = self.price_history[scope_length-1]
+
+
+    def update_stock(self, curr_gen, price):
+        """
+        Input: Integer - curr_gen, float - price
+        Output: None
+        Update information of the stock
+        """
+        self.price_history[self.scope_length + curr_gen] = price # Update Price
+        self.previous_data = self.get_stock_data(curr_gen)
+        self.average_scope = self.get_average_scope()
+
+
+    def get_stock_data(self, curr_gen): 
+        """
+        Input: Integer - curr_gen
+        Output: np.array(Float)
+        Get a series of previous tu of stock price
+        """
+        return self.price_history[curr_gen: curr_gen+self.scope_length]
+
+
+    def get_average_scope(self):
+        return np.average(self.previous_data)
 
 
     def __repr__(self) -> str:
-        return f'S{self.id}-(num:{self.num_shares})'
+        return f'S{self.name}-(num:{self.num_shares})'
 
 
 class Investor:
-    def __init__(self, name, wallet, emotion, death):
+    def __init__(self, name, wallet, emotion, death, shares, ann_mean, ann_sd):
         self.name = name
         self.wallet = wallet
-        self.ann = ANN()
+        self.ann = ANN(ann_mean, ann_sd)
         self.emotion = emotion
+        self.action = Action()
         self.current = 1
         self.death = death
-        self.shares = []
+        self.shares = shares
         self.time_to_die = False
     
+
     def aging(self):
         """
         Input: None, Output: None
@@ -91,6 +127,40 @@ class Investor:
         """
         self.current += 1
         self.time_to_die = self.current == self.death
+
+
+    def decide_action(self, market):
+        """
+        Input: [Stock] - market
+        Output: None
+        Decide Action
+        """
+        np.random.shuffle(market) # So that every individual will have different order
+
+        for stock in market:
+            val = self.ann.predict(stock.previous_data) # Predict based on scope of previous data of the stock
+
+            if -self.emotion <= val <= self.emotion: # Halt for this stock
+                continue
+            else:
+                if val > self.emotion: # Buy this stock
+                    if stock.average_scope * (1 + abs(val)) >= self.wallet:
+                        self.action.set_action(1, self.wallet, stock_name=stock.name)
+                    else:
+                        self.action.set_action(1, stock.average_scope * (1 + abs(val)), stock_name=stock.name)
+                    return # Since we decide to Buy already
+                else:
+                    # Sell
+                    if self.shares[stock.name]:
+                        share = np.random.choice(self.shares[stock.name])
+                        self.action.set_action(2, (1 + abs(val)) * share.price * (1-share.failed_selling/10), stock_share=share)
+                        return # Since we decide to Sell already
+                    else:
+                        # no shares...
+                        continue
+        # Since all stocks are halt thus this action is halt
+        self.action.set_action(0)
+
 
     def __str__(self) -> str:
         return f'I{self.name}'
@@ -117,18 +187,20 @@ class ANN():
 
     weights and bias are initalized using Standard Normal Distribution (https://numpy.org/doc/stable/reference/random/generated/numpy.random.randn.html)
     '''
-    def __init__(self):
-        self.input_weights = np.random.randn(30)
-        self.input_biases = np.random.randn(30)
-        self.hidden_weights = np.random.randn(30, 10)
-        self.hidden_biases = np.random.randn(10)
-        self.output_weights = np.random.randn(10)
-        self.output_bias = np.random.randn()
+    def __init__(self, mean=0, sd=0.1):
+        self.input_weights = np.random.normal(mean,sd,30)
+        self.input_biases = np.random.normal(mean,sd,30)
+        self.hidden_weights = np.random.normal(mean,sd,(30, 10))
+        self.hidden_biases = np.random.normal(mean,sd,10)
+        self.output_weights = np.random.normal(mean,sd,10)
+        self.output_bias = np.random.normal(mean,sd)
         self.total_weights = (self.input_weights.size + self.input_biases.size + self.hidden_weights.size + self.hidden_biases.size + self.output_weights.size + 1) # +1 for the output bias
 
     def predict(self, input_values):
         input_layer = np.maximum(input_values * self.input_weights + self.input_biases, 0) # ReLu()
         hidden_layer = np.maximum(input_layer @ self.hidden_weights + self.hidden_biases, 0) # ReLu()
+        # input_layer = np.tanh(input_values * self.input_weights + self.input_biases) # tanh()
+        # hidden_layer = np.tanh(input_layer @ self.hidden_weights + self.hidden_biases) # tanh()
         output_layer = np.tanh(hidden_layer @ self.output_weights + self.output_bias) # Tanh()
         return output_layer
     
@@ -196,37 +268,83 @@ class ANN():
         raise IndexError("Invalid weight index")
 
 
-def initialize_investors(num_ind, wallet, emotion, death_mean, death_sd):
+class Action():
+    encode = {0: "halt", 1: "buy", 2: "sell", None: "None"}
+
+    def __init__(self):
+        self.action_num = None
+        self.transaction = False
+        self.price = None
+        self.stock_share = None
+        self.stock_name = None
+        self.stock_index = None
+    
+
+    def set_action(self, action, price=None, stock_share=None, stock_name=None):
+        """
+        Input: Integer - action, Float - price, Share - stock_share, String - stock_name
+        Output: None
+        Set action
+        """
+        self.action_num = action
+        if action == 0 and stock_share != None and price != None:
+            print("ERROR: action is Hold but stock and price are inputted")
+        else:
+            if action != 0:
+                self.price = np.round(price,2)
+                if action == 1:
+                    self.stock_name = stock_name
+                elif action == 2:
+                    self.stock_share = stock_share
+
+
+    def reset(self):
+        self.action_num = None
+        self.transaction = False
+        self.price = None
+        self.stock_share = None
+        self.stock_name = None
+        self.stock_index = None
+
+
+    def __str__(self) -> str:
+        return f'A-{self.encode[self.action_num]}({"sell:$" + str(self.price) + ", share:"+ str(self.stock_share) + "," if self.action_num==2 else "ub:$" + str(self.price) + "," + "stock:" + self.stock_name + "," if self.action_num == 1 else "" if self.action_num != None else ""}{"T" if self.transaction else "F"})'
+
+    def __repr__(self) -> str:
+        return f'A-{self.encode[self.action_num]}(price:{self.price},stock_name:{self.stock_name},share:{self.stock_share},{"T" if self.transaction else "F"})'
+
+
+def initialize_investors(stock_names, num_ind, wallet=200, emotion=0.5, death_mean=75, death_sd=10, ann_mean=0, ann_sd=0.1):
     """
-    Input: Integer - num_ind, Float - wallet, Float - emotion, Integer - death_mean, Integer - death_sd
+    Input: Integer - num_ind, Float - wallet, Float - emotion, Integer - death_mean, Integer - death_sd, [String] - stock_names
     Output: np.Array(Investor)
     Initialize Investors
     """
-    investors = [Investor(i, wallet, emotion, int(np.random.normal(death_mean, death_sd))) for i in range(num_ind)]
+    investors = [Investor(i, wallet, emotion, int(np.random.normal(death_mean, death_sd)), {name: [] for name in stock_names}, ann_mean, ann_sd) for i in range(num_ind)]
     return np.array(investors)
 
 
-def initialize_stock(num_stocks=2, num_shares=100, num_gen=100, names=[], p_stock_mean=100, p_stock_sd=20, trend_length=30, p_share_sd=20):
+def initialize_stock(num_stocks=2, num_shares=100, num_gen=100, names=["ABC","XYZ"], p_stock_mean=100, p_stock_sd=20, scope_length=30, p_share_sd=10):
     """
     Input: Integer - num_stocks
-    Optional Inputs: Integer - num_shares, Integer - num_gen, [String] - names, Integer - p_stock_mean, Integer - p-stock_sd, Integer - trend_length, Integer - p_share_sd
+    Optional Inputs: Integer - num_shares, Integer - num_gen, [String] - names, Integer - p_stock_mean, Integer - p-stock_sd, Integer - scope_length, Integer - p_share_sd
     Output: np.array(Stock)
     Initialize Stocks
     """
     if len(names) == 0:
-        stocks = [Stock(i, num_shares, num_gen, p_stock_mean, p_stock_sd, trend_length, p_share_sd) for i in range(num_stocks)]
+        stocks = [Stock(i, num_shares, num_gen, p_stock_mean, p_stock_sd, scope_length, p_share_sd) for i in range(num_stocks)]
     else:
         if len(names) == num_stocks:
-            stocks = [Stock(i, num_shares, num_gen, p_stock_mean, p_stock_sd, trend_length, p_share_sd) for i in names] # len(names) == num_stocks should be true
+            stocks = [Stock(i, num_shares, num_gen, p_stock_mean, p_stock_sd, scope_length, p_share_sd) for i in names] # len(names) == num_stocks should be true
         else:
             print("ERROR: The length of names and num_stocks doesn't match!!!")
-            stocks = [Stock(i, num_shares, num_gen, p_stock_mean, p_stock_sd, trend_length, p_share_sd) for i in range(num_stocks)]
+            stocks = [Stock(i, num_shares, num_gen, p_stock_mean, p_stock_sd, scope_length, p_share_sd) for i in range(num_stocks)]
     return np.array(stocks)
 
 
-def assign_initial_shares(investors, stocks, num_directors, stage="general"):
+def assign_initial_shares(investors, stocks, num_directors, stage_threshold):
     """
-    Input: np.Array(Investor) - investors, np.Array(Investor) - stocks, String - stage
+    Input: np.Array(Investor) - investors, np.Array(Investor) - stocks, Integer - stage_threshold
     Output: None
     Assign shares of stock to investors based on the provided stage
     """
@@ -235,69 +353,40 @@ def assign_initial_shares(investors, stocks, num_directors, stage="general"):
         np.random.shuffle(stock.shares)
         np.random.shuffle(investors)
 
-        if stage == "early":
-            directors_ratio = 0.8
-            dir_share_ratio = 0.6
-            man_share_ratio = 0.2
-            dir_num_ratio = 0.4
-            man_num_ratio = 0.3 + dir_num_ratio
-        else:
-            # stage == "general"
-            if stage != "general":
-                print("ERROR: initialization stage should be general or early")
-            directors_ratio = 0.3
-            dir_share_ratio = 0.5
-            man_share_ratio = 0.3
-            dir_num_ratio = 0.2
-            man_num_ratio = 0.3 + dir_num_ratio
+        if stock.num_shares <= stage_threshold: # early stage
+            directors_share_ratio = 0.8
+            
+        else: # general stage
+            directors_share_ratio = 0.3
 
-        # Assign shares to the directors investors
-        # Get different Directors Share's Size
-        directors_size = int(directors_ratio * len(stock.shares))
-        dir_size = int(dir_share_ratio * directors_size)
-        man_size = int(man_share_ratio * directors_size)
-        emp_size = directors_size - dir_size - man_size
+        # Get Directors Shares and Normal Investors Shares
+        directors_size = int(np.round(directors_share_ratio * stock.num_shares))
+        directors_shares = stock.shares[:directors_size]
+        normal_shares = stock.shares[directors_size:]
 
-        # Get Investors as directors
-        directors = np.random.choice(len(investors), num_directors, replace=False)
-        num_dir = len(directors) # default should be 10% of total number of investors
-        dir_investors = investors[directors[:int(dir_num_ratio * num_dir)]]
-        man_investors = investors[directors[int(dir_num_ratio * num_dir): int(man_num_ratio * num_dir)]]
-        emp_investors = investors[directors[int(man_num_ratio * num_dir):]]
+        # Get Investors as directors and Normal Investors
+        directors = investors[:num_directors]
+        normal_inv = investors[num_directors:]
 
-        # Get different Directors Shares
-        dir_shares = stock.shares[:dir_size]
-        man_shares = stock.shares[dir_size:dir_size+man_size]
-        emp_shares = stock.shares[dir_size+man_size:dir_size+man_size+emp_size]
-
-        # Assign
-        for share in dir_shares:
-            share.update_owner(np.random.choice(dir_investors))
-        for share in man_shares:
-            share.update_owner(np.random.choice(man_investors))
-        for share in emp_shares:
-            share.update_owner(np.random.choice(emp_investors))
-        
-        
-        # Assign shares to the normal investors (uniformly distributed)
-        normal_shares = stock.shares[directors_size:] # Get shares for normal investors
-        normal_investors = investors[np.setdiff1d(np.arange(len(investors)),directors)] # Get normal investors
+        # Assign Directors Shares, Normal Investors Shares to directors, normal investors respectively
+        for share in directors_shares:
+            share.update_owner(np.random.choice(directors))
 
         for share in normal_shares:
-            share.update_owner(np.random.choice(normal_investors))
+            share.update_owner(np.random.choice(normal_inv))
 
 
 ### Test ANN ###
-# net = ANN()
+# net = ANN(0,0.3)
 # print(net.get_weights()) # test get_weights()
 # print(net.get_weights(False)) # test get_weights()
 
-### test predict()
-# input_values = [*range(1,31)]
-# input_values = np.random.randn(30) # maybe all layers use tanh is better?
-# print(input_values)
-# output = net.predict(input_values)
-# print(output)
+### test predict() # Basically ANN sd should be < 0.2
+# for i in range(10):
+#     net = ANN(0,0.1)
+#     input_values = np.random.normal(100,30,30)
+#     # print(input_values)
+#     print(net.predict(input_values))
 
 ### test get_weight(), add_weight(), and update_weight()
 # print("10th weight is: " + str(net.get_weight(10)))
@@ -307,16 +396,16 @@ def assign_initial_shares(investors, stocks, num_directors, stage="general"):
 # net.update_weight(10, 22)
 # print("10th weight is: " + str(net.get_weight(10)))
 
-### Test Investor ###
-# arron = Investor(1, 100, 0.5, 3)
-# print(arron)
+### Test Investor's aging###
+# arron = Investor(1, 100, 0.5, 3, {}, 0,0.1)
+# print(repr(arron))
 # arron.aging()
-# print(arron)
+# print(repr(arron))
 # arron.aging()
-# print(arron)
+# print(repr(arron))
 
 ### Test Stock ###
-# abc = Stock("ABC", 20, 50)
+# abc = Stock("ABC", 20, 50, 100,20, 30, 10)
 # print(abc)
 # print(abc.shares)
 # print(len(abc.shares))
@@ -331,7 +420,7 @@ def assign_initial_shares(investors, stocks, num_directors, stage="general"):
 # print(s0.history)
 
 ### Test initialize_investors() ###
-# population = initialize_investors(50, 100.0, 0.5, 75, 5)
+# population = initialize_investors(["ABC"],50, 100.0, 0.3, 75, 5)
 # print(population)
 
 ### Test initialize_stock() ###
@@ -343,14 +432,56 @@ def assign_initial_shares(investors, stocks, num_directors, stage="general"):
 #     print(stock.price_history)
 
 ### Test assign_initial_shares ###
-# population = initialize_investors(50, 100.0, 0.5, 75, 5)
-# market = initialize_stock(num_stocks=2, num_shares=20, num_gen=20, names=["ABC", "XYZ"])
-# assign_initial_shares(population, market, 10, "early")
+# names = ["ABC", "XYZ"]
+# population = initialize_investors(names,50, 100.0, 0.5, 75, 5)
+# market = initialize_stock(num_stocks=2, num_shares=20, num_gen=20, names=names)
+# assign_initial_shares(population, market, 5, 20)
 # for stock in market:
 #     print(stock)
-#     print(len(stock.shares))
 #     for i in stock.shares:
 #         print(i)
 #     print()
 # for i in population:
 #     print(repr(i))
+
+### Test Action ###
+# act = Action()
+# print(act)
+# s0 = Stock('ABC', num_shares=1, num_gen=20, p_stock_mean=100, p_stock_sd=1, scope_length=30, p_share_sd=1)
+
+# act.set_action(0, stock_share=s0.shares[0], price=50)
+# act.set_action(0)
+# print(act)
+# act.set_action(2, 100, stock_share=s0.shares[0]) # sell
+# print(act)
+# act.set_action(1, price=200, stock_name=s0.name) # buy
+# print(act)
+# act.reset()
+# print(act)
+
+### Test update_price in Stock and get_stock_data ###
+# market = initialize_stock(1, 5, 30, ["ABC"])
+# s0 = market[0]
+# print(s0.price_history)
+# s0.update_stock(0, 10.0)
+# s0.update_stock(1, 20.0)
+# print(s0.price_history)
+# print(s0.previous_data)
+# print(s0.previous_data.shape)
+# print(s0.average_scope)
+
+### Test Decide Action ###
+# market_names = ["ABC", "XYZ"]
+# market = initialize_stock(2, 10, 20, market_names)
+# population = initialize_investors(market_names,10, 500, 0.5, 75, 10)
+# assign_initial_shares(population, market, 2, 1)
+
+# for ind in population:
+#     print(repr(ind))
+#     print(ind.action)
+# print()
+# print("Decide Action")
+# for ind in population:
+#     ind.decide_action(market)
+#     print(repr(ind))
+#     print(ind.action)
--- backend/EA/main.py
+++ backend/EA/main.py
@@ -6,3 +6,152 @@ Disclaimer: This is for Queen's University CISC 455 Team Project
             Team member: E Ching Kho, Somoina Tian, Wenqi Tang
             Python Version 3.11.2 when developing
 """
+# Import Files and Libraries
+import numpy as np
+from initialization import Share, Stock, Investor, ANN, Action, initialize_investors, initialize_stock, assign_initial_shares
+
+def ea(pop_size, wallet, emotion, death_mean, death_sd, num_directors, market_names, num_shares, num_gen=30):
+    # Initialization
+    market = initialize_stock(len(market_names),num_shares, num_gen, market_names)
+    population = initialize_investors(market_names, pop_size, wallet, emotion, death_mean, death_sd)
+    assign_initial_shares(population, market, num_directors, 10)
+    
+    # Loop
+    for curr_gen in range(num_gen):
+        # Action Rundown
+        action_rundown(3, population, market, curr_gen)
+    
+    for stock in market:
+        print(stock)
+        print(stock.price_history)
+
+
+def get_stock_names(market):
+    """
+    Input: [Stock] - market
+    Output: [String]
+    Get All Stock name in the market
+    """
+    names = []
+    for stock in market:
+        names.append(stock.name)
+    return names
+
+
+def action_rundown(k, population, market, curr_gen):
+    for _ in range(k):
+        # Action Decide
+        for ind in population:
+            ind.decide_action(market)
+        # Intraday Trading
+        transactions = intraday_trading(population, get_stock_names(market))
+        # Update Stock's last price
+        for stock in market:
+            stock.last_price = np.round((np.sum(transactions[stock.name]) + (stock.num_shares - len(transactions[stock.name]))*stock.last_price)/stock.num_shares,2)
+        # Reset Every Indivdual's Action (Just in case even though it replaces the previous action)
+        for ind in population:
+            ind.action.reset()
+    
+    # Record this generation stocks closing price
+    for stock in market:
+        stock.update_stock(curr_gen, stock.last_price)
+
+
+def intraday_trading(population, market_names):
+    intraday = {stock: [[],[]] for stock in market_names} # {"stock": [[],[]]} index 0 is Individual who Buy, index 1 is Individual who Sell 
+    transactions = {stock: [] for stock in market_names} # Store Success Transactions' Sold Price
+
+    # Collect Individual based on Action
+    for ind in population:
+        if ind.action.action_num != 0:
+            if ind.action.action_num == None:
+                print("ERROR: action shouldn't be None")
+            else:
+                if ind.action.action_num == 1: # Indivdual with Buy Action
+                    intraday[ind.action.stock_name][0].append(ind)
+                elif ind.action.action_num == 2: # Indivdual with Sell Action
+                    intraday[ind.action.stock_share.stock.name][1].append(ind)
+                else:
+                    print("ERROR: action encoding should be just 0,1,2")
+
+    # Trading
+    for stock, value in intraday.items():
+        if value[1]: # There exists Seller
+            np.random.shuffle(value[0]) # Random Shuffle Buyer list
+            seller_price = [seller.action.price for seller in value[1]]
+            for buyer in value[0]:
+                if buyer.action.price >= np.min(seller_price): # Investor is smart and buy the cheapest available
+                    # Transaction success and record the sold price
+                    seller_index = np.argmin(seller_price)
+                    transactions[stock].append(buy(buyer, value[1][seller_index]))
+                    del value[1][seller_index] # remove from Seller list
+                    del seller_price[seller_index] # remove from seller_price list
+                    if not seller_price:
+                        break # no more individual selling
+
+            # Failed Selling++ for those remaining Sellers who can't sell
+            for seller in value[1]:
+                seller.action.stock_share.failed_selling += 1
+    
+    return transactions
+
+
+def buy(buyer, seller):
+    """
+    Input: Investor - buyer, Investor - seller
+    Output: float
+    Perform the transaction action between buyer and seller and Return the sold price
+    """
+    share = seller.action.stock_share # Get the share that are trading
+
+    # Trade Share
+    seller.shares[share.stock.name].remove(share) # Remove Share from Seller
+    buyer.shares[share.stock.name].append(share)
+
+    # Trade Money
+    sold_price = seller.action.price
+    seller.wallet += sold_price
+    buyer.wallet -= sold_price
+
+    # Update share's owner
+    share.update_owner_price(buyer, sold_price)
+
+    # Update Transaction's Status
+    seller.action.transaction = True
+    buyer.action.transaction = True
+
+    return sold_price
+
+
+### Test buy() ###
+# print("initialize")
+# pop = initialize_investors(["ABC"], 2, 50)
+# print(pop)
+# mar = initialize_stock(1, 1, 20, ["ABC"], 20, 2, 5, 1)
+# share = mar[0].shares[0] # share
+# print(share)
+# share.update_owner(pop[0])
+# print("assigned")
+# print(pop)
+# print(share)
+# pop[0].action.set_action(2, 20, stock_share=share)
+# pop[1].action.set_action(1, 25, stock_name="ABC")
+# print(pop[0].action)
+# print(pop[1].action)
+# print("buy")
+# print("sold price:" + str(buy(pop[1], pop[0])))
+# print(pop)
+# print(share.history)
+# print(pop[0].action)
+# print(pop[1].action)
+
+
+### Test Action Rundown
+# market_names = ["ABC", "XYZ"]
+# market = initialize_stock(2, 5, 20, market_names)
+# population = initialize_investors(10, 100, 0.5, 75, 10, market_names)
+# curr_gen = 0
+# assign_initial_shares(population, market, 2)
+
+### Test ea
+ea(50, 200.0, 0.3, 75, 5, 5, ["ABC","XYZ"], 20)
